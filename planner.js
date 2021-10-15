5b821a49324a48bdbc5ae775f2332209f2fcc63e
Lata Patel <71697573+niightOwlDev23@users.noreply.github.com>
2021-10-15 23:30:37 +0530

    Add files via upload

--- planner.js
+++ planner.js
@@ -6,4 +6,7 @@ $(document).ready(function() {
     $("#carouselExampleIndicators .close").click(function() {
         $('#carouselExampleIndicators').hide();
     });
+    $('.unplanned-order').excelTableFilter();
+    $('.planned-order').excelTableFilter();
+    $('.held-orders').excelTableFilter();
 })
